b1290644faee604bff784c83d1dbdecb73c160bd
Kras13 <paskalev.1989@abv.bg>
2022-07-29 00:04:57 +0300

    Add SuperHeroAPI project

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,24 @@
+﻿namespace SuperHeroAPI.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            //<List<SuperHero> -> return value...
+
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
